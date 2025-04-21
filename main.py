dcd3d5d8fa7e499b3a00329d7afce277f32eddd1
kelle <kellerhydle@gmail.com>
2025-04-20 19:58:04 -0600

    UI Updates
    -updated default window size

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ class SweepPC(QtWidgets.QMainWindow):
         super().__init__()
         self.setWindowTitle('SweepPC')
         self.setWindowIcon(QtGui.QIcon('resources/icons/sweep_pc_icon.svg'))
+        self.resize(1000, 800)
 
         self.createUI()
 
     def createUI(self):
         pass
 
+
 def main():
     app = QtWidgets.QApplication([])
 
     window = SweepPC()
     window.show()
-    window.showMaximized()
 
     app.exec()
 
