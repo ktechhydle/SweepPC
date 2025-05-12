4d587dddd4f0b7884934a131b44795c8f0a04f02
kelle <kellerhydle@gmail.com>
2025-05-12 10:13:45 -0600

    Major Updates
    -redefined project goals

--- src/gui/widgets.py
+++
@@ -1,7 +0,0 @@
-from src.imports import *
-
-
-class RunButton(QPushButton):
-    def __init__(self, text, parent=None):
-        super().__init__(text, parent)
-        self.setObjectName('runButton')
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,1 @@
-use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
 
-
-
-#[pyfunction]
-fn test() -> PyResult<()> {
-    println!("Hello from rust!");
-    Ok(())
-}
-
-#[pymodule]
-fn sweep_pc(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(test, py)?)?;
-    Ok(())
-}
