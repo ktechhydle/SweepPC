70f0c6bdfc66afe158a25b9c9b16b29ed5f3c37c
kelle <kellerhydle@gmail.com>
2025-05-20 10:50:42 -0600

    Evaluator Updates
    -code cleanup and optimizations

--- src/framework/evaluate.rs
+++ src/framework/evaluate.rs
@@ -1,49 +1,49 @@
 use crate::framework;
 use colored::Colorize;
 use std::fs;
-use std::io;
+use std::io::Result;
 
-pub fn evaluate_results(results: io::Result<Vec<String>>) {
-    match results {
-        Ok(found_files) => {
-            if !found_files.is_empty() {
-                let mut displayed_results = String::new();
-
-                for result in &found_files {
-                    displayed_results.push_str(&format!(
-                        "{} {}\n",
-                        "File:".italic().cyan(),
-                        result.to_string().replace("\\", "/")
-                    ));
-                }
-
-                if !displayed_results.is_empty() {
-                    println!(
-                        "\n{} {} {} \n\n{}",
-                        "SweepPC found".bold().green(),
-                        found_files.len().to_string().bold().green(),
-                        "result(s)".bold().green(),
-                        displayed_results
-                    );
-
-                    let delete = framework::get_answer::get_yes_or_no(
-                        "Would you like to delete the found files?",
-                    );
-
-                    if delete {
-                        for file in found_files {
-                            let _ = fs::remove_file(file);
-                        }
-                        println!("{}", "Deleted found files".green());
-                    }
-                }
-            } else {
-                println!(
-                    "{}",
-                    "SweepPC didn't find any files, your computer is clean 😊".white()
-                )
-            }
+pub fn evaluate_results(results: Result<Vec<String>>) {
+    let found_files = match results {
+        Ok(p) => p,
+        Err(_) => {
+            eprintln!("{}", "A specified directory was not found".red());
+            Vec::new()
         }
-        Err(e) => eprintln!("{} {}", "Error scanning directory:".red(), e),
+    };
+
+    if !found_files.is_empty() {
+        let mut displayed_results = String::new();
+
+        for result in &found_files {
+            displayed_results.push_str(&format!(
+                "{} {}\n",
+                "File:".italic().cyan(),
+                result.to_string().replace("\\", "/")
+            ));
+        }
+
+        println!(
+            "\n{} {} {} \n\n{}",
+            "SweepPC found".bold().green(),
+            found_files.len().to_string().bold().green(),
+            "result(s)".bold().green(),
+            displayed_results
+        );
+
+        let delete =
+            framework::get_answer::get_yes_or_no("Would you like to delete the found files?");
+
+        if delete {
+            for file in found_files {
+                let _ = fs::remove_file(file);
+            }
+            println!("{}", "Deleted found files".green());
+        }
+    } else {
+        println!(
+            "{}",
+            "SweepPC didn't find any files, your computer is clean 😊".white()
+        )
     }
 }
